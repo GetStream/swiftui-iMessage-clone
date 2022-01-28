f995b42f6da128de319b9a333d017328eef0fdb2
Stefan <stefan.blos@gmail.com>
2022-01-28 18:45:55 +0100

    Initial Commit

---
+++ iMessageClone/iMessageCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  iMessageCloneApp.swift
+//  iMessageClone
+//
+//  Created by Stefan Blos on 28.01.22.
+//
+
+import SwiftUI
+
+@main
+struct iMessageCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
