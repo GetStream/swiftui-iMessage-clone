93eb05626aa1e51f7d20269b8977a64700379c8c
Stefan <stefan.blos@gmail.com>
2022-06-29 14:09:28 +0200

    Implemented the Message View

--- iMessageClone/StreamChat/iMessageViewFactory+MessageList.swift
+++ iMessageClone/StreamChat/iMessageViewFactory+MessageList.swift
@@ -5,6 +5,7 @@
 //  Created by Stefan Blos on 29.06.22.
 //
 
+import SwiftUI
 import StreamChat
 import StreamChatSwiftUI
 
@@ -12,6 +13,27 @@ extension iMessageViewFactory {
     
     func makeChannelHeaderViewModifier(for channel: ChatChannel) -> MessageListHeaderModifier {
         MessageListHeaderModifier(channel: channel)
+    }
+    
+    func makeMessageTextView(
+        for message: ChatMessage,
+        isFirst: Bool,
+        availableWidth: CGFloat,
+        scrolledId: Binding<String?>
+    ) -> MessageView {
+        guard let userId = chatClient.currentUserId else {
+            return MessageView(message: message.text, isCurrentUser: false, isFirst: isFirst)
+        }
+        
+        return MessageView(message: message.text, isCurrentUser: userId == message.author.id, isFirst: isFirst)
+    }
+    
+    func makeMessageDateView(for message: ChatMessage) -> some View {
+        EmptyView()
+    }
+    
+    func makeMessageReadIndicatorView(channel: ChatChannel, message: ChatMessage) -> some View {
+        EmptyView()
     }
     
 }
