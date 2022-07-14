8677a00365253ef5f4e8d490b790163301fe3601
Stefan <stefan.blos@gmail.com>
2022-07-14 15:46:24 +0200

    Final touches for the video

---
+++ iMessageClone/MessageList/View/Attachments/LinkView.swift
@@ -0,0 +1,20 @@
+//
+//  LinkView.swift
+//  iMessageClone
+//
+//  Created by Stefan Blos on 29.06.22.
+//
+
+import SwiftUI
+
+struct LinkView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LinkView_Previews: PreviewProvider {
+    static var previews: some View {
+        LinkView()
+    }
+}
